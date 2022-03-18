0764ea81a06f7188da1ae8b307c5f2b8db86d71b
SENES <caro67200@gmail.com>
2022-03-18 01:40:41 +0100

    add newsletter

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,59 @@
+import styled from "@emotion/styled";
+import { Send } from "@mui/icons-material";
+
+const Container = styled.div`
+  height: 60vh;
+  background-color: #fcf5f5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+const Title = styled.h2`
+  font-size: 70px;
+  margin-bottom: 20px;
+`;
+const Desc = styled.div`
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+`;
+const InputContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgray;
+`;
+const Input = styled.input`
+  border: none;
+  flex: 8;
+  padding-left: 20px;
+`;
+const Button = styled.button`
+flex: 1;
+border: none;
+background-color: teal;
+color: white;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const Newsletter = () => {
+  return (
+    <Container>
+      <Title>Newsletter</Title>
+      <Desc>Soyez prévenu de l'arrivée de nouveaux produits.</Desc>
+      <InputContainer>
+        <Input placeholder="Email"></Input>
+        <Button>
+          <Send />
+        </Button>
+      </InputContainer>
+    </Container>
+  );
+};
+
+export default Newsletter;
